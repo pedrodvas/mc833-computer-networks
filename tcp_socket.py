93f5f56eae098551284c7911ff503ab83fb4ede6
Pedro Damasceno Vasconcellos <pedro.d.vas@gmail.com>
2026-09-07 22:42:04 -0300

    Add initial working version. File transfer will have to be slowed to show concurrency, and 404 still doesn't work properly

--- tcp_socket.py
+++ tcp_socket.py
@@ -2,29 +2,41 @@
 from socket import *
 import sys # In order to terminate the program
 import threading
+import mimetypes
+import os
+
 def serve_content(connectionSocket, addr):
     print(f"new connection at {connectionSocket}: {addr}")
     try:
-            message = connectionSocket.recv(1024).decode()
-            filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata = #Fill in start #Fill in end
-            #Send one HTTP header line into socket
-            #Fill in start
-            #Fill in end
-            #Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.close()
-        except IOError:
-            #Send response message for file not found
-            #Fill in start#Fill in end
-            #Close client socket
-            #Fill in start
-            #Fill in end
-            serverSocket.close()
-            sys.exit()#Terminate the program after sending the corresponding data
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        filepath = filename[1:] if filename != '/' else 'index.html'
+        full_filepath = os.path.join(r"C:\Users\pdvsp\Downloads", filepath)
+        with open(full_filepath, 'rb') as f:
+            outputdata = f.read()
+
+        content_type, _ = mimetypes.guess_type(full_filepath)
+        if content_type is None:
+            content_type = "application/octet-stream"
+        header = f"HTTP/1.1 200 OK\r\n"
+        header += f"Content-Type: {content_type}\r\n"
+        header += f"Content-Length: {len(outputdata)}\r\n"
+        header += "Connection: close\r\n\r\n"
+        
+        connectionSocket.sendall(header.encode())
+        connectionSocket.sendall(outputdata)
+        connectionSocket.close()
+    except IOError:
+        body = "<html><body><h1>404 Not Found</h1></body></html>\r\n"
+        header_404 = (
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-Type: text/html\r\n"
+            f"Content-Length: {len(body.encode())}\r\n"
+            "Connection: close\r\n\r\n"
+        )
+        connectionSocket.sendall(header_404.encode())
+        connectionSocket.sendall(body.encode())
+        connectionSocket.close()
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 serverPort = 6767 #which port 
